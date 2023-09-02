9ca75bc1138169e4d68a4c7aaa4ff73929f285db
VitorBafilli1 <143266924+VitorBafilli1@users.noreply.github.com>
2023-09-02 09:58:44 -0300

    teste
    teste 1  1

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
+﻿﻿/*******************VITOR*******************/
+//Vitor você vai fzer o codigo para o usuario digitar e guardar na variével
+//vão ser dois numeros digite o primeir depois digite o segundo.
+
+/*******************Alexandre*******************************************/
+//Alexandre vai escrever o codigo das operações operações
+//matemática soma, subtração, divisão e multiplicação
+
+/**************************************************************/
+ 
 ﻿int numero1 = 0; 
 int numero2 = 0;
 
