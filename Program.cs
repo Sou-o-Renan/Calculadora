0ac156e84305b17709a753151e90feaee3022550
EmersonProg <95104916+EmersonProg@users.noreply.github.com>
2023-09-02 09:53:25 -0300

    Tarefas para os programadores

--- Program.cs
+++ Program.cs
@@ -1,3 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, Emerson!");
+﻿/*******************VITOR*******************/
+//Vitor você vai fzer o codigo para o usuario digitar e guardar na variével
+//vão ser dois numeros digite o primeir depois digite o segundo.
+
+/*******************Alexandre*******************************************/
+//Alexandre vai escrever o codigo das operações operações
+//matemática soma, subtração, divisão e multiplicação
+
+/**************************************************************/
+ 
